dd667ef9588679eaf1e3371c474d5ff77dcf5c94
mco345 <mco345@skuniv.ac.kr>
2022-08-04 15:22:52 +0900

    First commit to connect with GitHub

--- app/src/main/java/com/example/showmyroom/fragment/HomeFragment.java
+++ app/src/main/java/com/example/showmyroom/fragment/HomeFragment.java
@@ -1,25 +1,39 @@
 package com.example.showmyroom.fragment;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.showmyroom.R;
+import com.example.showmyroom.activity.WriteHomeActivity;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
     private View writeButton;
 
+    // 앨범 이미지 선택
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef, pathRef;
+    private ArrayList<Uri> uriList = new ArrayList<>();
+
     // 갤러리 호출
-    final int REQUEST_TAKE_ALBUM = 1;
-    PermissionCheck permissionCheck;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -37,36 +51,60 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        permissionCheck = new PermissionCheck(getActivity());
+        storageRef = storage.getReference();
+
 
         writeButton = v.findViewById(R.id.writeFloatingButton);
         writeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getAlbum();
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 2222);
             }
         });
 
         return v;
     }
 
-    private void getAlbum(){
-        // 앨범 호출
-        boolean isAlbum = permissionCheck.isCheck("Album");
-        if(isAlbum){
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_PICK);
-                    intent.setType("image/*");
-                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                    intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                    startActivityForResult(Intent.createChooser(intent,"다중 선택은 '포토'를 선택하세요."), REQUEST_TAKE_ALBUM);
-                }catch(Exception e){
-                    Log.e("error", e.toString());
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) {
+            Toast.makeText(getActivity(), "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            if (data.getClipData() == null) {
+                Log.e(TAG, "single choice: "+String.valueOf(data.getData()));
+                Uri imageUri = data.getData();
+                uriList.add(imageUri);
+            } else {
+                ClipData clipData = data.getClipData();
+                Log.e("clipData", String.valueOf(clipData.getItemCount()));
+
+                if (clipData.getItemCount() > 10) {
+                    Toast.makeText(getActivity(), "사진은 10장까지 선택 가능합니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.e(TAG, "multiple choice");
+                    for(int i = 0; i < clipData.getItemCount(); i++){
+                        Uri imageUri = clipData.getItemAt(i).getUri();
+
+                        try{
+                            uriList.add(imageUri);
+                        }catch (Exception e){
+                            Log.e(TAG, "File select error", e);
+                        }
+                    }
                 }
             }
-            else{
-                Log.e("kitkat under", "..");
+            for(int i = 0; i<uriList.size(); i++){
+                pathRef = storageRef.child("Post/" + System.currentTimeMillis()+ "/"+ i + ".png");
             }
+            Intent intent = new Intent(getActivity(), WriteHomeActivity.class);
+            startActivity(intent);
+
         }
+    }
 }
